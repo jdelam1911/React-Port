08a3d168874dad77063608758be09244406aa0f9
jdelam1911 <145811498+jdelam1911@users.noreply.github.com>
2024-08-19 14:10:44 -0500

    added all files needed and code w it,fxing problems w connections

---
+++ src/components/About/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import profileImage from "../../assets/large/Profile/Profile_Pic.png";
+function About() {
+  return (
+    <section id="about-me" className="my-5 intro">
+      <div class="flex-row">
+        <h1 id="about" className="section-title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flex-row">
+        <div class="intro-info">
+          <div class="intro-img">
+            <img src={profileImage} style={{ width: "100%" }} alt="profile" />
+          </div>
+          <p>
+            Hey, hey, hey! I'm Juan Delatorre! I am a student under the program, KU Coding Boootcamp for Full Stack Dev! 
+          </p>
+          <p>test.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
